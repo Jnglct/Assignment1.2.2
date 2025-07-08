36fdbae194fc783113be24f89cdc58aca1b19dd2
Jnglct <rogerscole99@gmail.com>
2025-07-08 16:25:17 -0500

    Add project files.

---
+++ Assignment1.2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Assignment1._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int sum = 0;
+            Console.WriteLine("The first 10 natural number is: ");
+            for (int n = 1; n < 11; n++)
+            {
+                Console.Write(n + " ");
+                sum = n + i;
+                i = sum;
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("The Sum is: " + sum);
+        }
+    }
+}
